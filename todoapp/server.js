59f6eb0cb522eb3b55f3e498db58d4e9628b2895
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-06-05 18:51:23 +0900

    Initial commit

---
+++ todoapp/server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const GridFSBucket = require('mongodb/lib/gridfs-stream');
+const app =express();
+app.use(express.urlencoded({extended: true})) 
+const MongoClient = require('mongodb').MongoClient;
+app.set('view engine','ejs');
+
+app.use('/public', express.static('public'));
+
+var db;
+MongoClient.connect('mongodb+srv://leejiyoung:ofz9d2zr@boilerplate.xcwv7.mongodb.net/?retryWrites=true&w=majority',
+function (에러, client) {
+	if (에러) return console.log(에러)
+	db = client.db('todoapp');
+
+        // db.collection('post').insertOne( {이름 : 'John', _id : 100} , function(에러, 결과){
+	    // console.log('저장완료'); 
+	// });
+
+	app.listen(8080, function () {
+		console.log('listening on 8080')
+	});
+});
+
+
+app.get('/pet',function(요청,응답){
+    응답.send('pet용품 쇼핑 할 수 있는 사이트 입니다')
+});
+
+app.get('/',function(요청,응답){
+    응답.sendFile(__dirname+'/index.html')
+});
+
+app.get('/write',function(요청,응답){
+    응답.sendFile(__dirname+'/write.html')
+});
+
+app.post('/add', function (요청, 응답) {
+    db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
+      var 총게시물갯수 = 결과.totalPost
+  
+      db.collection('post').insertOne({ _id : 총게시물갯수 + 1, 제목 : 요청.body.title, 날짜 : 요청.body.date }, function (에러, 결과) {
+        db.collection('counter').updateOne({name:'게시물갯수'},{ $inc: {totalPost:1} },function(에러, 결과){
+      if(에러){return console.log(에러)}
+          응답.send('전송완료');
+        })
+      })
+  
+    })
+  })
+app.get('/list',function(요청,응답){
+ db.collection('post').find().toArray(function(에러,결과){
+     console.log(결과);
+     응답.render('list.ejs',{ posts:결과 });
+ });
+});
+
+app.delete('/delete', function(요청, 응답){
+    요청.body._id = parseInt(요청.body._id)
+    db.collection('post').deleteOne(요청.body, function(에러, 결과){
+      console.log('삭제완료');
+      응답.status(200).send({ message : '성공했스무니다'});
+    })
+  });
+
+  app.get('/detail/:id', function(요청, 응답){
+    db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
+      응답.render('detail.ejs', {data : 결과} )
+    })
+  });
+
+
