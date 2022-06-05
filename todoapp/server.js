47bd159cff87054bcbbca1c1199ed408c829a6ae
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-06-05 21:36:55 +0900

    (Member authentication function 2)
     Compare ID and password with DB and create a session

--- todoapp/server.js
+++ todoapp/server.js
@@ -101,3 +101,29 @@ app.post('/login', passport.authenticate('local', {
   응답.redirect('/')
 });
 
+passport.use(new LocalStrategy({
+  usernameField: 'id',
+  passwordField: 'pw',
+  session: true,
+  passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+  // console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+});
+
+passport.deserializeUser(function (아이디, done) {
+  done(null, {})
+}); 
