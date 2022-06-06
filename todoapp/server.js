cee8c8fc14d4f130420aa0619852f51b7c1098a5
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-06-06 19:07:05 +0900

    Sign Up-2
    unique ID -Add author when uploading

--- todoapp/server.js
+++ todoapp/server.js
@@ -34,17 +34,6 @@ app.get('/write',function(요청,응답){
   응답.render('write.ejs');
 });
 
-app.post('/add', function (요청, 응답) {
-    db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
-      var 총게시물갯수 = 결과.totalPost
-      db.collection('post').insertOne({ _id : 총게시물갯수 + 1, 제목 : 요청.body.title, 날짜 : 요청.body.date }, function (에러, 결과) {
-        db.collection('counter').updateOne({name:'게시물갯수'},{ $inc: {totalPost:1} },function(에러, 결과){
-      if(에러){return console.log(에러)}
-          응답.send('전송완료');
-        })
-      })
-    })
-  })
 app.get('/list',function(요청,응답){
  db.collection('post').find().toArray(function(에러,결과){
      console.log(결과);
@@ -173,6 +162,21 @@ app.post('/register',function(요청,응답){
   })
 })
 
+
+
+app.post('/add', function (요청, 응답) {
+
+  응답.send('전송완료');
+  db.collection('counter').findOne({ name: '게시물갯수' }, function (에러, 결과) {
+    var 총게시물갯수 = 결과.totalPost;
+    var post = { _id: 총게시물갯수 + 1, 작성자: 요청.user._id , 제목: 요청.body.title, 날짜: 요청.body.date }
+    db.collection('post').insertOne( post , function (에러, 결과) {
+      db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
+        if (에러) { return console.log(에러) }
+      })
+    });
+  });
+});
 
 // app.use('/shop',require('./routes/shop.js'));
 // app.use('/board/sub',require('./routes/board.js'));
