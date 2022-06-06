42561fa3ff7f53694775928965af23e3c931f534
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-06-06 19:39:48 +0900

    Router
    Only logged in people can access

--- todoapp/routes/shop.js
+++ todoapp/routes/shop.js
@@ -1,10 +1,20 @@
 var router = require('express').Router();
 
-router.get('/shirts', function(요청, 응답){
+function 로그인했니(요청,응답,next){
+    if(요청.user){
+      next()
+    }else{
+      응답.send('로그인안하셨는데욤?')
+    }
+  }
+
+ router.use(로그인했니);
+
+router.get('/shirts', 로그인했니,function(요청, 응답){
    응답.send('셔츠 파는 페이지입니다.');
 });
 
-router.get('/pants', function(요청, 응답){
+router.get('/pants', 로그인했니,function(요청, 응답){
    응답.send('바지 파는 페이지입니다.');
 }); 
 
